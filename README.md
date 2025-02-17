e125bd4d3a0da6a77e42f993535193c3889cf66d
Filipe Lima Guedes Chagas <filipechgs@gmail.com>
2025-02-17 11:18:44 -0300

    Atualiza readme e ABB.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Estrutura de dados em Java
 Estruturas de dados codificadas em Java, conforme o livro "Estruturas de Dados" de Valter Castelhano de Oliveira.
+
+## Como usar
+1. Instale o JDK na sua máquina
+2. Abra o terminal integrado da IDE, ou seu terminal de preferência
+3. Va para o diretório da estrutura de dados que você quer testar, ex.: cd arvore-binaria-busca/
+4. Compile o arquivo a partir do terminal, ex.: jovac ExemploABB.java 
+5. Execute o programa com o seguinte comando: java ExemploABB.java
--- arvore-binaria-busca/ArvoreBinariaBusca.java
+++ arvore-binaria-busca/ArvoreBinariaBusca.java
@@ -38,12 +38,12 @@ public class ArvoreBinariaBusca {
         }
     }
 
-    /* Algoritmos de varredura */
-    private void inFixado(No atual) // caminhamento in fixado da árvore binária
+    /* Algoritmos de varredura e busca*/
+    private void inFixado(No atual) // caminhamento in fixado da árvore binária. É o mesmo que simFixado
     {
         if (atual != null) {
             inFixado(atual.getEsq());
-            System.out.println("Id: " + atual.getId() + " Elemento: "+atual.getElemento());
+            System.out.println("Id: " + atual.getId() + " Elemento: " + atual.getElemento());
             inFixado(atual.getDir());
         }
     }
@@ -61,6 +61,128 @@ public class ArvoreBinariaBusca {
             }
         }
         return a;
+    }
+
+    private No busca(No atual, long id) // busca recursiva na ABB
+    {
+        if (atual == null) { // não encontrou e retorna nulo
+            return null;
+
+        } else {
+            if (id == atual.getId()) { // achou o elemento
+                return atual;
+
+            } else {
+                if (id < atual.getId()) {
+                    // busca só na subárvore esquerda
+                    return busca(atual.getEsq(), id);
+
+                } else {
+                    // busca só na subárvore direita
+                    return busca(atual.getDir(), id);
+                }
+            }
+        }
+    }
+
+    public No buscaABB(long id) // busca de um elemento na ABB
+    {
+        return busca(raiz, id);
+    }
+
+    public boolean removeABB(long id) // remove um elemento da ABB
+    { // a ABB não pode ser vazia
+        No atual = raiz;
+        No pai = raiz;
+        boolean filhoEsq = true;
+        while (atual.getId() != id) { // busca o elemento
+            pai = atual;
+            if (id < atual.getId()) { // busca à esquerda
+                filhoEsq = true;
+                atual = atual.getEsq();
+            } else { // busca à direita
+                filhoEsq = false;
+                atual = atual.getDir();
+            }
+            if (atual == null) { // não achou e termina
+                return false;
+            }
+        }
+        
+        // caso 1: elemento não possui filhos
+        if (atual.getEsq() == null && atual.getDir() == null) {
+            if (atual == raiz) { // eliminando a raiz da ABB
+                raiz = null;
+        
+            } else {
+                if (filhoEsq) { // o elemento era filho esquerdo
+                    pai.setEsq(null);
+        
+                } else { // o elemento era filho direito
+                    pai.setDir(null);
+                }
+            }
+        } else {
+            if (atual.getDir() == null) { // Caso 2: com apenas o filho esquerdo
+                if (atual == raiz) { // eliminando a raiz
+                    raiz = atual.getEsq();
+        
+                } else {
+                    if (filhoEsq) { // o elemento era filho esquerdo
+                        pai.setEsq(atual.getEsq());
+        
+                    } else { // o elemento era filho direito
+                        pai.setDir(atual.getEsq());
+                    }
+                }
+            } else {
+                if (atual.getEsq() == null) { // Caso 2: com apenas o filho direito
+                    if (atual == raiz) { // eliminando a raiz
+                        raiz = atual.getDir();
+        
+                    } else {
+                        if (filhoEsq) { // o elemento era filho esquerdo
+                            pai.setEsq(atual.getDir());
+        
+                        } else { // o elemento era filho direito
+                            pai.setDir(atual.getDir());
+                        }
+                    }
+                } else { // Caso 3: elemento possui os dois filhos
+                    No sucessor = getSucessor(atual); // busca o elemento sucessor
+                    if (atual == raiz) { // raiz passa a ser o sucessor
+                        raiz = sucessor;
+        
+                    } else {
+                        if (filhoEsq) { // o elemento era filho esquerdo
+                            pai.setEsq(sucessor);
+        
+                        } else { // o elemento era filho direito
+                            pai.setDir(sucessor);
+                        }
+                    }
+                    sucessor.setEsq(atual.getEsq());
+                }
+            }
+        }
+        return true;
+    }
+
+    private No getSucessor(No eliminado) // encontra o próximo menor valor
+    {
+        No sucessorPai = eliminado;
+        No sucessor = eliminado;
+        No atual = eliminado.getDir(); // ir para o filho da direita
+        while (atual != null) { // até não haver mais filhos à esquerda
+            sucessorPai = sucessor;
+            sucessor = atual;
+            atual = atual.getEsq();
+        }
+        if (sucessor != eliminado.getDir()) { // se não for o próprio filho direito
+            sucessorPai.setEsq(sucessor.getDir());
+            sucessor.setDir(eliminado.getDir());
+        }
+        return sucessor;
     }
 
     public long alturaArvore() {
